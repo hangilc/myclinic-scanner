0b1346337733c39bc90877f452a11d5db2b6dd7c
hangilc <hangil@chang.jp>
2017-01-11 11:12:55 +0900

    initial commit

---
+++ Scanner/Scanner/Main.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Scanner
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startScanButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // startScanButton
+            // 
+            this.startScanButton.Location = new System.Drawing.Point(13, 13);
+            this.startScanButton.Name = "startScanButton";
+            this.startScanButton.Size = new System.Drawing.Size(75, 23);
+            this.startScanButton.TabIndex = 0;
+            this.startScanButton.Text = "スキャン開始";
+            this.startScanButton.UseVisualStyleBackColor = true;
+            this.startScanButton.Click += new System.EventHandler(this.startScanButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.startScanButton);
+            this.Name = "Form1";
+            this.Text = "スキャナー";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startScanButton;
+    }
+}
+
---
+++ Scanner/Scanner/Main.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WIA;
+
+namespace Scanner
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void startScanButton_Click(object sender, EventArgs e)
+        {
+            WIA.CommonDialog dlg = new WIA.CommonDialog();
+            Device d = dlg.ShowSelectDevice(WiaDeviceType.ScannerDeviceType, true, false);
+            if (d == null)
+            {
+                return;
+            }
+            Item item = d.Items[1];
+            ImageFile image = dlg.ShowTransfer(item, FormatID.wiaFormatJPEG, false) as ImageFile;
+            image.SaveFile("scanned.jpeg");
+        }
+    }
+}
